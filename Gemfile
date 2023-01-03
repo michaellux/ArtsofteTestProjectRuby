918d33fd38c6ece696f06c99e10a7b4a337ee44c
Sidachenko Michael <michaellux@yandex.ua>
2023-01-03 21:32:10 +0300

    Start add logic for delete employee place and autocomplete

--- Gemfile
+++ Gemfile
@@ -42,6 +42,10 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+#for name field autocomplete and delete items
+gem 'jquery-rails'
+gem "jquery-ui-rails"
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get "add", to: "list_employee#add", as: :add
+  get "names" => "list_employee#names", as: "names"
   get "edit", to: "list_employee#edit"
   post "add", to: "list_employee#create"
   patch "edit", to: "list_employee#update"
+  delete "delete", to: "list_employee#destroy"
 
   get "department", to: "department#add"
   post "department", to: "department#create"
